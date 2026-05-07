9292049f296af25fc11b5125773e017d386046bd
Terry <terry.douteau17@gmail.com>
2026-05-07 14:34:13 +0200

    Modification de la classe compte pour faire fonctionner l'affichage différencié

--- lib/Classes/compte.dart
+++ lib/Classes/compte.dart
@@ -3,12 +3,14 @@ class Compte {
   final String nom;
   final String prenom;
   final String identifiant;
+  final int poste;
 
   Compte({
     required this.mail,
     required this.nom,
     required this.prenom,
     required this.identifiant,
+    required this.poste,
   });
 
   factory Compte.fromJson(Map<String, dynamic> json) {
@@ -17,6 +19,7 @@ class Compte {
       nom: json['Nom']?.toString() ?? '',
       prenom: json['Prenom']?.toString() ?? '',
       identifiant: json['Identifiant']?.toString() ?? '',
+      poste: json['IdPoste'] ?? '',
     );
   }
 
@@ -26,10 +29,11 @@ class Compte {
       'Nom': nom,
       'Prenom': prenom,
       'Identifiant': identifiant,
+      'IdPoste': poste,
     };
   }
 
   @override
   String toString() =>
-      'Compte{nom: $nom, prenom: $prenom, mail: $mail, identifiant: $identifiant}';
+      'Compte{nom: $nom, prenom: $prenom, mail: $mail, identifiant: $identifiant, idPoste: $poste}';
 }
