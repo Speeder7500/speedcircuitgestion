e3391b4c553959051c761a36f4f827313dbce73e
Terry <terry.douteau17@gmail.com>
2026-05-12 16:35:34 +0200

    Modification des réservation screen en cours

--- lib/Classes/reservation.dart
+++ lib/Classes/reservation.dart
@@ -1,12 +1,14 @@
 class Reservation {
   final int numSession;
-  final DateTime heure;
+  final DateTime date;
+  final String heure;
   final String vehicule;
   final String nom;
   final String prenom;
 
   Reservation({
     required this.numSession,
+    required this.date,
     required this.heure,
     required this.vehicule,
     required this.nom,
@@ -14,9 +16,22 @@ class Reservation {
   });
 
   factory Reservation.fromJson(Map<String, dynamic> json) {
+    final rawDate = json['DateSession'].toString().split('T')[0];
+    final parts = rawDate.split('-');
+    final date = DateTime(
+      int.parse(parts[0]),
+      int.parse(parts[1]),
+      int.parse(parts[2]),
+    );
+
+    final rawHeure = json['HeureReservation'].toString();
+    final heureParts = rawHeure.split(':');
+    final heure = '${heureParts[0]}:${heureParts[1]}';
+
     return Reservation(
-      numSession: json['IdSession'],
-      heure: DateTime.parse(json['DateSession']),
+      numSession: json['NumSession'],
+      date: date,
+      heure: heure,
       vehicule: '${json['Marque']} ${json['Modele']}',
       nom: json['Nom'],
       prenom: json['Prenom'],
@@ -26,8 +41,9 @@ class Reservation {
   Map<String, dynamic> toJson() {
     return {
       'IdSession': numSession,
-      'DateSession': heure.toIso8601String(),
-      'vehicule': vehicule,
+      'DateSession': date.toIso8601String(),
+      'HeureReservation': heure,
+      'Vehicule': vehicule,
       'Nom': nom,
       'Prenom': prenom,
     };
@@ -35,5 +51,5 @@ class Reservation {
 
   @override
   String toString() =>
-      'Reservation{id: $numSession, client: $prenom $nom, vehicule: $vehicule}';
+      'Reservation{id: $numSession, client: $prenom $nom, vehicule: $vehicule, date: $date, heure: $heure}';
 }
